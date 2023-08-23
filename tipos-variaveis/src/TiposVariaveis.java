f1a06de98f0f68bdfe36b69572a09a78e86c006c
Wallace-Winkler <wallace.html66@gmail.com>
2023-08-22 20:42:00 -0400

    Tipos de variaveis

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,3 +1,20 @@
 public class TiposVariaveis {
+    public static void main (String[] args){}
+    String nome = "Wallace";
+    // deve ser colocado um caractere de acordo com a tabela ascii
+    char estrela = 83;
+    //short serve para numeros pequenos
+    short memoryCardTam = 128;
+    // mais usados para numeros pois comporta um tamanho bom de algarismos
+    int idade = 32;
+    // long é usados para numeros muito grandes onde o int não comporta ex quantidade de capital dos estados unidos
+    long quantidadedeEstrelas = 24000000000000L;
+    // float é um numero decimal com até 4 bytes
+    float precoCamisa = 67.50F;
+    // double numeros decimal com ate 8 bytes
+    double precoJaqueta = 250.99;
+
+    // Final é uma constante nao muda
+    final String MEU_NOME = "Wallace";
     
 }
